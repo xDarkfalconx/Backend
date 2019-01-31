56bd1641ae5fc657e1635e02fc45314309a82762
xDarkfalconx <Julian.e.oquendo@gmail.com>
2019-01-30 21:25:56 -0800

    Entity user created

---
+++ demo/src/main/java/com/youtube/demo/model/User.java
@@ -0,0 +1,73 @@
+package com.youtube.demo.model;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+@Access(AccessType.FIELD)
+
+public class User extends ParentEntity {
+
+	private static final long serialVersionUID = 8435405591663518471L;
+	
+	@Column(name="first_name",nullable=false,length=255)
+	private String firstname;
+	
+	@Column(name="second_name",nullable=true,length=255)
+	   private String secondname;
+	
+	@Column(name="first_surname",nullable=false,length=255)
+		private	String firstsurname;
+	
+	@Column(name="second_surname",nullable=true,length=255)
+		private	String secondsurname; 
+	
+	@Column(name="phone",nullable=true,length=255)
+		private	String phone;
+	
+	@Column(name="adress",nullable=false,length=255)
+		private	String adress;
+		
+		public String getFirstname() {
+			return firstname;
+		}
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+		public String getSecondname() {
+			return secondname;
+		}
+		public void setSecondname(String secondname) {
+			this.secondname = secondname;
+		}
+		public String getFirstsurname() {
+			return firstsurname;
+		}
+		public void setFirstsurname(String firstsurname) {
+			this.firstsurname = firstsurname;
+		}
+		public String getSecondsurname() {
+			return secondsurname;
+		}
+		public void setSecondsurname(String secondsurname) {
+			this.secondsurname = secondsurname;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getAdress() {
+			return adress;
+		}
+		public void setAdress(String adress) {
+			this.adress = adress;
+		} 
+	
+
+}
